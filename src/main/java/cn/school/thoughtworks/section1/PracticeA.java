a209c771e5548ad512d1e84a150e0bde87191b47
niubilityxiesong <563527588@qq.com>
2018-04-02 15:47:19 +0800

    first

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -1,10 +1,30 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+
+        if(collection1.size() > collection2.size()){
+            return (SameArry(collection1, collection2));
+        }
+        else{
+            return (SameArry(collection2, collection1));
+        }
+
+    }
+
+    List<String> SameArry(List<String> longarry, List<String> shortarry){
+        int i,j;
+        List<String> resultarry = new ArrayList();
+        for(i = 0; i < longarry.size(); i++){
+            for(j = 0; j < shortarry.size(); j++){
+                if(longarry.get(i) == shortarry.get(j)){
+                    resultarry.add(shortarry.get(j));
+                }
+            }
+        }
+        return resultarry;
     }
 }
