9de35cf2535dc69d5098a9ea7fc070629ed6dff2
niubilityxiesong <563527588@qq.com>
2018-04-03 10:10:59 +0800

    S3PA

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -1,12 +1,26 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeA {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
+        List<String> changeobject = new ArrayList();
+        int temp;
 
-        return null;
+        for(int i = 0; i < object.get("value").size(); i++){
+            changeobject.add(object.get("value").get(i));
+        }
+
+        for(int j = 0; j < changeobject.size(); j++){
+            if(collectionA.containsKey(changeobject.get(j))){
+                temp = collectionA.get(changeobject.get(j));
+                --temp;
+                collectionA.put(changeobject.get(j), temp);
+            }
+        }
+        return collectionA;
     }
 }
